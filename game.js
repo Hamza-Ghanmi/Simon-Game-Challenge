233236f5cb3bc44ae6bc0d2d262cf79f24ba977c
Hamza-Ghanmi <ghanmi7amza@gmail.com>
2021-10-30 02:42:07 +0200

    next start the game

---
+++ game.js
@@ -0,0 +1,34 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+
+
+
+
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + '.mp3');
+    audio.play();
+}
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+} 
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
